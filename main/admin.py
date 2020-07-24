e48189ee8db1949c41deed03086f13dd32e8ad4e
Innocent Zenda <zendainnocent@gmail.com>
2020-07-24 15:00:43 +0300

    Add method for getting ending amount

--- main/admin.py
+++ main/admin.py
@@ -5,8 +5,10 @@ from .models import Transaction
 
 class TransactionManager(admin.ModelAdmin):
     list_display = ('id', 'date', 'name', 'amount',
-                    'payment_type', 'account', 'status',)
+                    'payment_type', 'account', 'status',
+                    'starting_amount', 'ending_amount',)
     list_filter = ('date', 'account', 'status', )
+    search_fields = ('name', 'date',)
 
 
 admin.site.register(Transaction, TransactionManager)
--- main/models.py
+++ main/models.py
@@ -31,6 +31,22 @@ class Transaction(models.Model):
     account = models.CharField(max_length=3, choices=ACCOUNT_CHOICES)
     status = models.CharField(max_length=2, choices=STATUS_CHOICES)
     date = models.DateField(auto_now_add=True)
+    starting_amount = models.FloatField(default=0)
+    ending_amount = models.FloatField(default=0)
 
     def __str__(self):
         return self.name
+
+    def get_ending_amount(self):
+        ending_amount = self.starting_amount
+
+        if self.status == 'W':
+            ending_amount -= self.amount
+        elif self.status == 'D':
+            ending_amount += self.amount
+
+        return ending_amount
+
+    def save(self, *args, **kwargs):
+        self.ending_amount = self.get_ending_amount()
+        return super().save(*args, **kwargs)
