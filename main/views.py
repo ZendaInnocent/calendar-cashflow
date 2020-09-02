564c20963f82c1e96c6ca30e29037186f2f6c1e4
Innocent Zenda <zendainnocent@gmail.com>
2020-09-02 18:01:56 +0300

    refactor month_detail_view

--- main/views.py
+++ main/views.py
@@ -16,104 +16,60 @@ def index(request):
 
 
 def month_detail_view(request, year, month):
+    q = Transaction.objects.values('date')
+    context = {}
 
-    if request.method == "POST":
-        q = Transaction.objects.values('date')
-        start_day = request.POST['start-day'].upper()
+    if request.method == 'POST':
+        # start_day = request.POST['start-day'].upper()
         month = request.POST['month']
         year = request.POST['year']
-
-        d = datetime.date(int(year), int(month), 1)
-        cal = CashFlowCalendar(transactions=q, month=month, year=year)
-
-        previous_month = datetime.date(year=d.year, month=d.month, day=1)
-        previous_month = previous_month - datetime.timedelta(days=1)
-        previous_month = datetime.date(
-            year=previous_month.year, month=previous_month.month, day=1)
-
-        last_day = calendar.monthrange(d.year, d.month)
-        next_month = datetime.date(year=d.year, month=d.month, day=last_day[1])
-        next_month = next_month + datetime.timedelta(days=1)
-        next_month = datetime.date(
-            year=next_month.year, month=next_month.month, day=1)
-
-        for i in range(0, 7):
-            if calendar.day_name[i].upper() == start_day:
-                day = i
-
-        cal.setfirstweekday(day)
-
-        html_calendar = cal.formatmonth(d.year, d.month, withyear=True)
-        html_calendar = html_calendar.replace(
-            '<table ', '<table class="table table-bordered"')
-
-        context = {
-            'days_names': [day for day in calendar.day_name],
-            'months_names': {i: calendar.month_name[i] for i in range(1, 13)},
-            'years': [i for i in range(2019, 2022)],
-            'previous_month': reverse(
-                'main:month-detail', kwargs={
-                    'year': previous_month.year, 'month': previous_month.month}
-                    ),
-            'current_month': reverse(
-                'main:month-detail', kwargs={
-                    'year': d.year, 'month': d.month}),
-            'next_month': reverse(
-                'main:month-detail', kwargs={
-                    'year': next_month.year, 'month': next_month.month}),
-            'selected_day': calendar.day_name[day].capitalize(),
-            'month_name': calendar.month_name[d.month].capitalize(),
-            'selected_month': d.month,
-            'selected_year': d.year,
-            'html_calendar': html_calendar
-        }
-
+        # for i in range(0, 7):
+        #     if calendar.day_name[i].upper() == start_day:
+        #         day = i
         return redirect(reverse('main:month-detail', kwargs={
             'year': year, 'month': month}), context)
 
-    elif request.method == 'GET':
-        q = Transaction.objects.values('date')
-        d = datetime.date(
-            year=year, month=month, day=1)
+    day = 0
+    selected_day = calendar.day_name[day].capitalize()
 
-        cal = CashFlowCalendar(transactions=q, year=year, month=month)
-        day = 0
+    d = datetime.date(int(year), int(month), 1)
+    cal = CashFlowCalendar(transactions=q, year=year, month=month)
+    cal.setfirstweekday(day)
 
-        previous_month = datetime.date(year=d.year, month=d.month, day=1)
-        previous_month = previous_month - datetime.timedelta(days=1)
-        previous_month = datetime.date(
-            year=previous_month.year, month=previous_month.month, day=1)
+    previous_month = datetime.date(year=d.year, month=d.month, day=1)
+    previous_month = previous_month - datetime.timedelta(days=1)
+    previous_month = datetime.date(
+        year=previous_month.year, month=previous_month.month, day=1)
 
-        last_day = calendar.monthrange(d.year, d.month)
-        next_month = datetime.date(year=d.year, month=d.month, day=last_day[1])
-        next_month = next_month + datetime.timedelta(days=1)
-        next_month = datetime.date(
-            year=next_month.year, month=next_month.month, day=1)
+    last_day = calendar.monthrange(d.year, d.month)
+    next_month = datetime.date(year=d.year, month=d.month, day=last_day[1])
+    next_month = next_month + datetime.timedelta(days=1)
+    next_month = datetime.date(
+        year=next_month.year, month=next_month.month, day=1)
 
-        html_calendar = cal.formatmonth(d.year, d.month, withyear=True)
-        html_calendar = html_calendar.replace(
-            '<table ', '<table class="table table-bordered"')
+    html_calendar = cal.formatmonth(d.year, d.month, withyear=True)
+    html_calendar = html_calendar.replace(
+        '<table ', '<table class="table table-bordered"')
 
-        context = {
-            'days_names': [day for day in calendar.day_name],
-            'months_names': {i: calendar.month_name[i] for i in range(1, 13)},
-            'years': [i for i in range(2019, 2022)],
-            'previous_month': reverse(
-                'main:month-detail', kwargs={
-                    'year': previous_month.year, 'month': previous_month.month}
-                    ),
-            'current_month': reverse(
-                'main:month-detail', kwargs={
-                    'year': d.year, 'month': d.month}),
-            'next_month': reverse(
-                'main:month-detail', kwargs={
-                    'year': next_month.year, 'month': next_month.month}),
-            'selected_day': calendar.day_name[day].capitalize(),
-            'month_name': calendar.month_name[d.month].capitalize(),
-            'selected_month': d.month,
-            'selected_year': d.year,
-            'html_calendar': html_calendar
-        }
+    context = {
+        'month_name': calendar.month_name[d.month].capitalize(),
+        'selected_day': selected_day,
+        'selected_month': d.month,
+        'selected_year': d.year,
+        'days_names': [day for day in calendar.day_name],
+        'months_names': {i: calendar.month_name[i] for i in range(1, 13)},
+        'years': [i for i in range(2019, 2022)],
+        'html_calendar': html_calendar,
+        'previous_month': reverse('main:month-detail', kwargs={
+                            'year': previous_month.year,
+                            'month': previous_month.month}),
+        'current_month': reverse('main:month-detail', kwargs={
+                            'year': d.year, 'month': d.month}),
+        'next_month': reverse('main:month-detail', kwargs={
+                            'year': next_month.year,
+                            'month': next_month.month}),
+
+    }
 
     return render(request, 'main/month_detail.html', context)
 
